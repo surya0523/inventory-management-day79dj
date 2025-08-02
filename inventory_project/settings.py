cea118bf1a2202989184243ab968a8f3ca239232
surya0523 <surya23051997@gmail.com>
2025-08-02 10:47:18 +0530

    Add Render URL to ALLOWED_HOSTS

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['inventory-management-day79dj.onrender.com', '127.0.0.1']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Add this line
